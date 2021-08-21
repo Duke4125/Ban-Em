fe9e23fab8eb0464766f7570977c2241f50ae1e3
Duke <alfieduke@icloud.com>
2021-08-21 09:08:48 +0100

    0.8

--- src/main/java/bangui/me/duke/Me/Duke.java
+++ src/main/java/bangui/me/duke/Me/Duke.java
@@ -3,9 +3,17 @@ package bangui.me.duke.Me;
 import bangui.me.duke.Me.commands.BanGUICommand;
 import bangui.me.duke.Me.commands.HelpCommand;
 import bangui.me.duke.Me.listeners.BanInvListener;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import bangui.me.duke.Me.commands.ReloadCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Consumer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public final class Duke extends JavaPlugin {
 
@@ -49,7 +57,7 @@ public final class Duke extends JavaPlugin {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(ChatColor.BLUE + "BAN EM: All working! - VER.0.7");
+        System.out.println(ChatColor.BLUE + "BAN EM: All working! - VER.0.8");
 
         getConfig().options().copyDefaults();
         saveDefaultConfig();
@@ -65,9 +73,19 @@ public final class Duke extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new BanInvListener(this), this);
 
+        Logger logger = this.getLogger();
+
+        new UpdateChecker(this, 95286).getVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                logger.info("There is not a new update available.");
+            } else {
+                logger.info(ChatColor.RED + "There is a new update available, Download here https://www.spigotmc.org/resources/ban-em-the-easy-ban-gui-plugin.95286/");
+                logger.info(ChatColor.RED + "Why should you update? Keeping the plugin upto date will get you tons of cool features and can fix bugs!");
+            }
+        });
     }
 
-    @Override
+
     public void onDisable() {
         // Plugin shutdown logic
     }
